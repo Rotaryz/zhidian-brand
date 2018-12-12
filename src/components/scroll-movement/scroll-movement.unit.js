c083a0cb33f6adbe960281013c7127e9049df855
jtr <jtr354@foxmail.com>
2018-12-12 10:35:43 +0800

    调试build

---
+++ src/components/scroll-movement/scroll-movement.unit.js
@@ -0,0 +1,7 @@
+import ScrollMovement from './scroll-movement'
+
+describe('@components/scroll-movement', () => {
+  it('exports a valid component', () => {
+    expect(ScrollMovement).toBeAComponent()
+  })
+})
