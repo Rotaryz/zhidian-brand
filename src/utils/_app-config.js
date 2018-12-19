f3223af6815b82af0fc8b868792717e433057a1b
jtr <jtr354@foxmail.com>
2018-12-19 10:40:29 +0800

    配置文件

--- src/utils/_app-config.js
+++ src/utils/_app-config.js
@@ -1,7 +1,7 @@
 const version = ``
 module.exports = {
   app: '赞播平台',
-  env: 'production',
-  api: 'https://zhidian-api.jkweixin.com' + version,
-  upload: 'https://zhidian-api.jkweixin.com' + version
+  env: 'release',
+  api: 'https://zhidian-api.jkweixin.net' + version,
+  upload: 'https://zhidian-api.jkweixin.net' + version
 }
