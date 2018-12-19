85d18d4b93a7ef58d0473ad4397c72cb4122e719
jtr <jtr354@foxmail.com>
2018-12-19 10:58:51 +0800

    配置文件

--- src/utils/_app-config.js
+++
@@ -1,7 +0,0 @@
-const version = ``
-module.exports = {
-  app: 'ME时代',
-  env: 'release',
-  api: 'https://zhidian-me-era-api.jkweixin.net' + version,
-  upload: 'https://zhidian-me-era-api.jkweixin.net' + version
-}
