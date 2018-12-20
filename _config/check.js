eebaf8fbcc9e13378165be66347684e72035f0c4
jtr <jtr354@foxmail.com>
2018-12-20 21:35:03 +0800

    检查版本工具

--- _config/check.js
+++ _config/check.js
@@ -5,20 +5,21 @@ const {GIT} = require('./constant')
 
 // 获取git分支
 let gitBranch = fs.readFileSync('.git/HEAD', 'utf-8').trim().replace('ref: refs/heads/', '')
-console.log(gitBranch)
-
 let appConfigPath = path.resolve(__dirname,'../src/utils/_app-config.js')
-let content = fs.readFileSync(appConfigPath,'utf-8')
-let api = content.match(/api:.*,/)[0].split(/('|"|`)/)[2].trim()
-let version = content.match(/version.*/) ? content.match(/version.*/)[0].split('=')[1].replace(/('|"|`)/g, '').trim() : ''
-console.log(api + version)
-let apiVersion = api + version
+let api = _readApi(appConfigPath)
 
 for(let key in GIT) {
-  console.log(key)
-  console.log(gitBranch)
   if (key === gitBranch) {
-    console.log(key)
-    console.log(apiVersion)
+    let flag = _readApi(GIT[key])=== api
+    if (flag) break
+    throw new Error('分支和版本运行不一致请执行 npm run init')
   }
+}
+
+function _readApi(url) {
+  let appConfigPath = path.resolve(url)
+  let content = fs.readFileSync(appConfigPath,'utf-8')
+  let api = content.match(/api:.*,/)[0].split(/('|"|`)/)[2].trim()
+  let version = content.match(/version.*/) ? content.match(/version.*/)[0].split('=')[1].replace(/('|"|`)/g, '').trim() : ''
+  return api + version
 }
