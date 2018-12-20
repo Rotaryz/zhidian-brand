0233f55db3c84d1ff94be49a1fb458446a18e322
jtr <jtr354@foxmail.com>
2018-12-20 21:37:11 +0800

    检查版本工具

--- src/utils/_app-config.js
+++ src/utils/_app-config.js
@@ -2,6 +2,6 @@ const version = ``
 export default {
   app: 'ME时代',
   env: 'production',
-  api: 'https://zhidian-me-era-api.jkweixin.com' + version,
+  api: 'https://zhidian-me-era-api.jkweixin.net' + version,
   upload: 'https://zhidian-me-era-api.jkweixin.com' + version
 }
