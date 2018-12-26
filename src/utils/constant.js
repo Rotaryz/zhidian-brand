470151a88701409759befcecca5f4d0f3b0bf687
jtr <jtr354@foxmail.com>
2018-12-26 17:38:50 +0800

    名词修改多店

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
 export const DEFAULT_LOGO = `https://zhidian-img.jkweixin.com/100002%2F2018%2F12%2F13%2F1544689190588-0.8299367197845957`
 export const SEVER_HPONE = `18688843109`
 export const DEFAULT_STR = '未知'
-export const MER_NAME_ARR = ['单店', '品牌加盟']
+export const MER_NAME_ARR = ['单店', '多店']
 export const BRANCH_ARR = ['总店', '分店']
 export const DEFAULT_MIDDLE_FN = 'defaultMiddleWare'
